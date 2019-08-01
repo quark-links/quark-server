9b74fb0766075fa66c12136c69b9b346725f46d9
jake@jakew.me <hi@jakew.me>
2019-08-01 15:37:43 +0100

    Add License and License Headers

--- src/main/java/uk/vh7/exception/ShortLinkNotFoundException.java
+++ src/main/java/uk/vh7/exception/ShortLinkNotFoundException.java
@@ -1,3 +1,20 @@
+/*
+ *     This file is part of VH7.
+ *
+ *     VH7 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     VH7 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with VH7.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package uk.vh7.exception;
 
 import org.springframework.http.HttpStatus;
